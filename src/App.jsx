4e73b90661099d9489b2843519182b9516f2ab9c
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-07 16:17:51 -0400

    Definición de variables y funciones

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,26 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const hp = 218 * 1.36;
+
+  function kwtohp(kw) {
+    return kw * 1.36;
+  }
 
   return (
     <section className="container my-2">
-      <p>Layout</p>
+      <h1>My car</h1>
+      <p>My car has {218 * 1.36} horsepower</p>
+      <p>Again, my car has {hp} horsepower</p>
+
+      <hr/>
+
+      <h1>My other car</h1>
+      <p>My other car has {kwtohp(218)} horsepower</p>
+
+
     </section>
   )
 }
